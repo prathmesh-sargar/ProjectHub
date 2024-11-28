367db17ad2f9a2f9df76f58031433aca26a2497e
prathmeshTech07005 <sargarprathmesh007@gmail.com>
2024-11-28 14:44:32 +0530

    second

--- src/components/Login.js
+++ src/components/Login.js
@@ -78,7 +78,7 @@ const Login = () => {
                       value={username}
                       onChange={(e) =>setusername(e.target.value)}
                       className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
-                      type="user"
+                      type="text"
                       placeholder="Enter  username"
                     ></input>
                   </div>
